1259561334cb1c9d8a2ec878aac0bf0286df16e8
dmitrijgulacenkov <pingvin1959@yandex.ru>
2019-11-01 17:37:15 +0300

    Main fix

--- question/views.py
+++ question/views.py
@@ -3,35 +3,33 @@ from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.http import HttpResponse
 from question import models
 
+
 # Create your views here.
 
 def one_question(request, name):
-
     try:
-        id=models.Article.objects.get(title=name).pk
+        title=models.Article.objects.get(pk=name).title
     except models.Article.DoesNotExist:
-        return (render(request,"./question/404.html"))
+        return render(request, "./question/404.html")
 
-
-    answers=models.Answer.objects.by_question(id)
-    paginated_data=paginate(answers,request)
-    rendered_data = {"questions": paginated_data, "title": name}
-    return render(request, "./question/one_question.html",rendered_data)
-
+    answers = models.Answer.objects.by_question(name)
+    paginated_data = paginate(answers, request)
+    rendered_data = {"questions": paginated_data, "title": title}
+    return render(request, "./question/one_question.html", rendered_data)
 
 
 def index_html(request):
     # return HttpResponse(year, content_type="text/plain")
 
     articles = models.Article.objects.new_published()
-    paginated_data=paginate(articles,request)
-    rendered_data={"questions":paginated_data}
+    paginated_data = paginate(articles, request)
+    rendered_data = {"questions": paginated_data}
 
-    return render(request, "./question/index.html",rendered_data)
+    return render(request, "./question/index.html", rendered_data)
 
 
 def login_html(request):
-    return render(request,"./question/login.html")
+    return render(request, "./question/login.html")
 
 
 def register_html(request):
@@ -39,20 +37,20 @@ def register_html(request):
 
 
 def add_question_html(request):
-    return render(request,"./question/add_question.html")
+    return render(request, "./question/add_question.html")
 
 
-def questions_by_tag_html(request,tag="kek"):
+def questions_by_tag_html(request, tag="kek"):
     data_list = list()
     for i in range(1, 10):
         data_list.append({"name": tag + " " + str(i)})  # заглушка
     data = {
         "questions": data_list}
 
-    paginated_data=paginate(data["questions"],request)
+    paginated_data = paginate(data["questions"], request)
 
-    rendered_data={"questions": paginated_data,"m_tag":tag}
-    return render(request,"./question/questions_bt_tag.html",rendered_data)
+    rendered_data = {"questions": paginated_data, "m_tag": tag}
+    return render(request, "./question/questions_bt_tag.html", rendered_data)
 
 
 def hot_questions_html(request):
@@ -64,7 +62,7 @@ def hot_questions_html(request):
 
 
 def paginate(objects_list, request):
-    paginator=Paginator(objects_list,3)
+    paginator = Paginator(objects_list, 3)
 
     page = request.GET.get('page')
     try:
@@ -76,7 +74,3 @@ def paginate(objects_list, request):
     except EmptyPage:
         contacts = paginator.page(paginator.num_pages)
     return contacts
-
-
-
-
