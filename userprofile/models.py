a98f70c797ba9123c53bd04eadf76dfaff1acc0e
dmitrijgulacenkov <pingvin1959@yandex.ru>
2019-11-02 18:20:51 +0300

    Версия с добавлением аватара в юзера

---
+++ userprofile/models.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*-
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(upload_to='images/users', verbose_name='Изображение')
+
+    def __unicode__(self):
+        return self.user
+
+    class Meta:
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
